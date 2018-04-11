55ada034c24154fdefd16e688136c48b7b26041d
Jon Martin Heiberg <jonma@Jons-MacBook-Pro.local>
2018-04-11 13:26:54 +0200

    clean map test and custom markers

---
+++ map.php
@@ -0,0 +1,318 @@
+<?php /* Template name: GoogleMaps Testing */ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var pos_center = {lat: 59.9187791, lng: 10.7491923};
+          
+          var POI = [
+              {
+                  position: new google.maps.LatLng(59.9162093, 10.7599091),
+                  type: 'wschool',
+                  name: 'fjerdingen'
+              },
+              {
+                  position: new google.maps.LatLng(59.9233303, 10.7521104),
+                  type: 'wschool',
+                  name: 'vulkan'
+              },
+              {
+                  position: new google.maps.LatLng(59.9112117,10.7426654),
+                  type: 'kschool',
+                  name: 'kristiania'
+              },
+              {
+                  position: new google.maps.LatLng(59.9221521,10.7519091),
+                  type: 'food',
+                  name: 'mathallen'
+              }
+          ]
+          
+          var iconPath = "/wp-content/themes/Divichild/img/";
+          var icons = {
+              wschool: {
+                  icon: iconPath + 'wlogo400x400.jpg',
+              },
+              kschool: {
+                  icon: iconPath + 'klogo960x960.png'
+              }
+          };
+          
+          
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: pos_center,
+            styles: [
+                  {
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "administrative",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "administrative",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "administrative.land_parcel",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "landscape",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi",
+                    "elementType": "labels.text",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.attraction",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.attraction",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.attraction",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.business",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.business",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.government",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.government",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.medical",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.medical",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.park",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.park",
+                    "elementType": "labels.text",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.place_of_worship",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.place_of_worship",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.school",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.school",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.sports_complex",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.sports_complex",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.arterial",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.highway",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "off"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.local",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.local",
+                    "elementType": "labels",
+                    "stylers": [
+                      {
+                        "visibility": "on"
+                      }
+                    ]
+                  }
+                ]
+        }); // end maps
+          
+        
+          
+        var markFjerdingen = new google.maps.Marker({
+          position: POI["0"].position,
+          map: map,
+          icon: { url: icons.wschool.icon,
+                 scaledSize: new google.maps.Size(50,50)}
+        });
+          var markVulkan = new google.maps.Marker({
+              position: POI["1"].position,
+              map: map,
+              icon: {url: icons.wschool.icon,
+                     scaledSize: new google.maps.Size(50,50)}
+          });
+          var markKristiania = new google.maps.Marker({
+              position: POI["2"].position,
+              map: map,
+              icon: {url: icons.kschool.icon,
+                     scaledSize: new google.maps.Size(50,50)}
+          });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcEPRn3WzY8AXDvnFP_WIgVTfbXodNhU4&callback=initMap">
+    </script>
+  </body>
+</html>

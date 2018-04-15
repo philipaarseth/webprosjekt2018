1b71c2fd04f75887f0b2ffe3ab5625356c1d8b58
alm97 <31214834+alm97@users.noreply.github.com>
2018-04-15 15:32:01 +0200

    directions-service.php
    testing directions service

---
+++ directions-service.php
@@ -0,0 +1,55 @@
+<?php /* Template name: Andreas Testing */ ?>
+
+<?php echo "andreas testing";
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>Directions service</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var fjerdingen = {lat: 59.9162093, lng: 10.7599091};
+        var kristiania = {lat: 59.9112117, lng: 10.7426654};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: fjerdingen,
+          zoom: 7
+        });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        });
+
+        // Set destination, origin and travel mode.
+        var request = {
+          destination: kristiania,
+          origin: fjerdingen,
+          travelMode: 'DRIVING'
+        };
+
+        // Pass the directions request to the directions service.
+        var directionsService = new google.maps.DirectionsService();
+        directionsService.route(request, function(response, status) {
+          if (status == 'OK') {
+            // Display the route on the map.
+            directionsDisplay.setDirections(response);
+          }
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnmyT7iUyNklKrx2LFCMVQMxw8PQDm5B4&callback=initMap">
+    </script>
+  </body>
+</html>

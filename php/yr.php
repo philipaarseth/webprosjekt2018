cfbe6b7d526dd4a4b4b7b9754f063bf8ea6fce46
tordjon <tordjon@gmail.com>
2018-05-22 13:48:55 +0200

    Changed yr cache

--- php/yr.php
+++ php/yr.php
@@ -52,42 +52,8 @@ function getXML($wppath2, $url){
 
   // api req
   $yrXML = file_get_contents($url);
-  $response = simplexml_load_string($yrXML);
-  $json = json_encode($response);
-
-
-  $fh = fopen($cacheFile, 'w');
-  fwrite($fh, time() . "\n");
-  fwrite($fh, $json);
-  fclose($fh);
-
-  return $json;
-}
-
-
-function getrealXML($wppath2, $url){
-
-  $cacheFile = $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/divichild/cache/' . md5($url);
-
-  if (file_exists($cacheFile)) {
-      $fh = fopen($cacheFile, 'r');
-      $cacheTime = trim(fgets($fh));
-
-      // if data was cached recently, return cached data
-      if ($cacheTime > strtotime('-10 minutes')) {
-          return fread($fh, 999999);
-      }
-
-      // else delete cache file
-      fclose($fh);
-      unlink($cacheFile);
-  }
-
-
-  // api req
-  $yrXML = file_get_contents($url);
-  $response = simplexml_load_string($yrXML);
-  $json = json_encode($response);
+//  $response = simplexml_load_string($yrXML);
+  //$json = json_encode($response);
 
 
   $fh = fopen($cacheFile, 'w');
@@ -134,7 +100,7 @@ function getJSON($wppath2, $url){
 
 if($_POST['time']){
   $time = $_POST['time']; // "2018-05-22T13:00:00Z";
-  $yrXML = file_get_contents($yrURL); //getrealXML($wordpressPath, $yrURL); //file_get_contents("https://api.met.no/weatherapi/locationforecast/1.9/?lat=59.9111522&lon=10.7450746");
+  $yrXML = getXML($wordpressPath, $yrURL); //file_get_contents("https://api.met.no/weatherapi/locationforecast/1.9/?lat=59.9111522&lon=10.7450746");
   $response = simplexml_load_string($yrXML);
   $xml = new SimpleXMLElement($yrXML);
 
@@ -148,9 +114,13 @@ if($_POST['time']){
   header('Content-Type: application/json; charset=utf-8');
   echo "[".$temp.",".$symbol."]";
 }else{
+
+
   $result = getXML($wordpressPath, $yrURL);
   header('Content-Type: application/json; charset=utf-8');
-  echo $result;
+  $response = simplexml_load_string($result);
+  $json = json_encode($response);
+  echo $json;
 
 }
 
